823594cdf2ce6ff33e561d24ce37d598009a4bae
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-16 19:34:17 +0000

    add check-response

---
+++ www/js/check-response.js
@@ -0,0 +1,27 @@
+window.checkResponse = function(data, status, xhr) {
+  console.log('check', data, status, xhr);
+  var msg = {type: 'success', message: "Success"};
+  if (data === null) return msg;
+  if (data instanceof Document) {
+    var $data = $(data);
+    if ($data.find('error').length) {
+      var code = $data.find('code').text();
+      var message = $data.find('error message').text();
+      msg = {type: 'danger', message: code + ': ' + message};
+    }
+  } else if (typeof data === 'object') {
+    var err = data.code && data.message;
+    if (err) msg = {type: 'danger', message: data.code + ': ' + data.message};
+    if (window.RECIPE && data.objectType === 'KalturaUiConfListResponse') {
+      data.objects = data.objects.filter(function(uiConf) {
+        return (uiConf.html5Url || '').indexOf('/v2') !== -1 || uiConf.objType === KalturaUiConfObjType.KRECORD;
+      });
+      if (!data.objects.length) return {type: 'danger', message: 'No v2 uiConfs found.'}
+    }
+  }
+  if (msg.message.indexOf('SERVICE_FORBIDDEN') !== -1) {
+    $('#KalturaLogin').modal('show');
+  }
+  return msg;
+}
+
