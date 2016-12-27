2af76cf54bca19f7e137e066ab8aa26591bb6ea0
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-27 19:45:51 +0000

    add show/hide sidebar functionality

---
+++ javascript/sidebar.js
@@ -0,0 +1,15 @@
+window.showKalturaSidebar = function() {
+  window.jquery('#KalturaSidebar .sidebar-content').show();
+  window.jquery('#KalturaSidebar .hide-sidebar').show();
+  window.jquery('#KalturaSidebar .show-sidebar').hide();
+  window.jquery('#KalturaSidebar').css('width', '300px');
+  window.jquery('app .container-fluid').css('padding-left', '345px')
+}
+
+window.hideKalturaSidebar = function() {
+  window.jquery('#KalturaSidebar .sidebar-content').hide();
+  window.jquery('#KalturaSidebar .hide-sidebar').hide();
+  window.jquery('#KalturaSidebar .show-sidebar').show();
+  window.jquery('#KalturaSidebar').css('width', '54px');
+  window.jquery('app .container-fluid').css('padding-left', '94px')
+}
