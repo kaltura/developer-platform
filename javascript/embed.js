9b724bdbcdfbf1183e2ce29efae3bb5e2bf9d068
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-28 19:53:10 +0000

    refactor embed and nav views

---
+++ javascript/embed.js
@@ -0,0 +1,6 @@
+if (!window.lucybot.embed) {
+  window.jquery(document).ready(function() {
+    window.jquery('body, app .container-fluid, .loading-screen').addClass('spaced-for-kaltura-nav');
+  });
+}
+
