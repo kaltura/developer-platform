161dd7a61d7ca5fd12405b2880d723e6636aad17
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-28 20:40:46 +0000

    fix embed styles

--- javascript/embed.js
+++
@@ -1,6 +0,0 @@
-if (!window.lucybot.embed) {
-  window.jquery(document).ready(function() {
-    window.jquery('body, app .container-fluid, .loading-screen').addClass('spaced-for-kaltura-nav');
-  });
-}
-
