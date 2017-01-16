b91b99643a13d510df9b902aab649c74beea6c8e
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-16 23:09:41 +0000

    maybe checkout orphan branch on gh-pages

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -3,15 +3,9 @@ git config user.name "Travis CI"
 git config user.email "bobby.brennan@gmail.com"
 
 git fetch origin gh-pages
-git checkout gh-pages
+git checkout gh-pages || git checkout --orphan gh-pages
 
 mv www/* ./
-
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
 git add .
 git commit -m "Deploy to GitHub Pages"
 
