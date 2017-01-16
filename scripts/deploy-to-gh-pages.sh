cab9e599eabd75c71f0c0f123ab4e058d212a1be
Bobby Brennan <bobby.brennan@gmail.com>
2017-01-16 23:34:56 +0000

    add some debugging logs to travis

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,8 +2,15 @@ set -e
 git config user.name "Travis CI"
 git config user.email "bobby.brennan@gmail.com"
 
+ls -lah ./www
+
+echo "Lucy DIR"
+ls -lah /home/travis/.nvm/versions/node/v7.4.0/lib/node_modules/lucy-v3
+
 git fetch origin gh-pages:gh-pages
 git checkout gh-pages
+
+git status
 
 mv www/* ./
 git add .
