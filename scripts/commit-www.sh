4b653090aae6c3fd78840059aca12b7985abdb1a
Bobby Brennan <bobby.brennan@gmail.com>
2017-02-11 19:26:12 +0000

    start commiting to www folder

--- scripts/commit-www.sh
+++ scripts/commit-www.sh
@@ -2,16 +2,9 @@ set -e
 git config user.name "Travis CI"
 git config user.email "bobby.brennan@gmail.com"
 
-git fetch origin gh-pages:gh-pages
-git checkout gh-pages
-
-echo "Commiting to gh-pages"
-rm -rf ./api-docs
-cp -r ./www/* ./
-rm -rf www/
-rm -rf node_modules/
+echo "Commiting build"
 git status
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Build www/"
 
 git push -u https://$GITHUB_ACCESS_TOKEN@github.com/bobby-brennan/kaltura-recipes-v3 gh-pages
