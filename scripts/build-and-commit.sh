4d9f8f4e1161db06c974f87dfb6148d1608ab8bc
Travis CI <bobby.brennan@gmail.com>
2017-02-12 00:56:06 +0000

    add skip ci to travis commits

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -7,6 +7,6 @@ lucybot build --prerender
 echo "Commiting build"
 git status
 git add .
-git commit -m "Build www/"
+git commit -m "Build www/ [skip ci]"
 
 git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/bobby-brennan/kaltura-recipes-v3 HEAD:master >> /dev/null
