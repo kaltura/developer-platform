22dcb76ff2913119208322a250da5f1cc16c7798
Jess Portnoy <jess.portnoy@kaltura.com>
2017-02-24 20:40:11 +0000

    .

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -8,5 +8,5 @@ echo "Commiting build"
 git status
 git add www/*
 git commit -a -m "Build www/ [skip ci]"
-git pull --no-edit
+#git pull --no-edit
 git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform HEAD:master >> /dev/null 2>&1
