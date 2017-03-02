283b39078846168539c854c85c2a6b6aa14b61f2
Travis CI <bobby.brennan@gmail.com>
2017-03-02 17:59:30 +0000

    fix git ident config

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -1,13 +1,12 @@
 set -e
-git config user.name "Travis CI"
-git config user.email "bobby.brennan@gmail.com"
 
 git clone https://github.com/kaltura/developer-platform-generated
 rm -rf developer-platform-generated/www
-
 lucybot build --prerender --destination ./developer-platform-generated/www
 
 cd developer-platform-generated
+git config user.name "Travis CI"
+git config user.email "bobby.brennan@gmail.com"
 echo "Commiting build"
 git status
 git add www/*
