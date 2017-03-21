f7881e266c9158f2e5c4bb993940868da67c922e
Jess Portnoy <jess.portnoy@kaltura.com>
2017-03-21 15:45:24 +0200

    following https://github.com/kaltura/developer-platform/commit/24a6279177db48fea3e62aad82c4319b704c2cc5

--- test/golden/with_arguments/ajax.js
+++ test/golden/with_arguments/ajax.js
@@ -10,7 +10,7 @@ pager.pageSize = 3;
 KalturaMediaService.listAction(filter, pager)
   .execute(client, function(success, results) {
     if (!success || (results && results.code && results.message)) {
-      console.log('Kaltura Error', results);
+      console.log('Kaltura Error', success, results);
     } else {
       console.log('Kaltura Result', results);
     }
