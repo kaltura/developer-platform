9d69d89a0b5bd73391889dd85a83955f00d57986
Travis CI <bobby.brennan@gmail.com>
2017-03-25 18:35:14 +0000

    use default_branch for support_matrix

--- scripts/resources/support_matrix.sh
+++ scripts/resources/support_matrix.sh
@@ -1,1 +1,2 @@
-curl "https://raw.githubusercontent.com/kaltura/clients-generator/Lynx-12.13.0/support_matrix.md" > ./markdown/generated/support_matrix.md
+BRANCH=`curl https://api.github.com/repos/kaltura/clients-generator -s |grep default_branch| sed 's/"default_branch":\s*"\(.*\)",/\1/' | sed 's@\s*@@g'`
+curl "https://raw.githubusercontent.com/kaltura/clients-generator/$BRANCH/support_matrix.md" > ./markdown/generated/support_matrix.md
