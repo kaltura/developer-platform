9f131462f543953d9baa52db4bd629db4c64bcf9
Travis CI <bobby.brennan@gmail.com>
2017-03-25 18:36:14 +0000

    better sed command for default_branch

--- scripts/resources/support_matrix.sh
+++ scripts/resources/support_matrix.sh
@@ -1,2 +1,2 @@
-BRANCH=`curl https://api.github.com/repos/kaltura/clients-generator -s |grep default_branch| sed 's/"default_branch":\s*"\(.*\)",/\1/' | sed 's@\s*@@g'`
+BRANCH=`curl https://api.github.com/repos/kaltura/clients-generator -s |grep default_branch| sed 's/\s*"default_branch":\s*"\(.*\)",/\1/'`
 curl "https://raw.githubusercontent.com/kaltura/clients-generator/$BRANCH/support_matrix.md" > ./markdown/generated/support_matrix.md
