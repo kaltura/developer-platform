1bfb3fe58ff03e3abf3b6f8f41a47fba399fa1f9
Travis CI <bobby.brennan@gmail.com>
2017-04-04 17:22:37 +0000

    disable vpaas for now

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -6,21 +6,21 @@ git config --global user.email "bobby.brennan@gmail.com"
 mkdir ./markdown/generated
 ./scripts/resources/all.sh
 
-echo "Building VPaaS Website..."
-git clone https://github.com/kaltura/developer-platform-generated generated/vpaas
-rm -rf generated/vpaas/*
-TARGET_API=vpaas lucybot build --prerender --destination generated/vpaas
+#echo "Building VPaaS Website..."
+#git clone https://github.com/kaltura/developer-platform-generated generated/vpaas
+#rm -rf generated/vpaas/*
+#TARGET_API=vpaas lucybot build --prerender --destination generated/vpaas
 
 echo "Building OTT Website..."
 git clone https://github.com/kaltura/ott-developer-platform-generated generated/ott
 rm -rf generated/ott/*
 TARGET_API=ott lucybot build --prerender --destination generated/ott
 
-cd generated/vpaas
-echo "Commiting build"
-git add .
-git commit -a -m "Build site"
-git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
+#cd generated/vpaas
+#echo "Commiting build"
+#git add .
+#git commit -a -m "Build site"
+#git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
 
 cd ../ott
 echo "Commiting build"
