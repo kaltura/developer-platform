ff103bdf0e97f6cb10325dff54e323d44ceb4af0
Travis CI <bobby.brennan@gmail.com>
2017-04-04 18:06:45 +0000

    re-enable VPaaS

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -6,25 +6,24 @@ git config --global user.email "bobby.brennan@gmail.com"
 mkdir ./markdown/generated
 ./scripts/resources/all.sh
 
-#echo "Building VPaaS Website..."
-#git clone https://github.com/kaltura/developer-platform-generated generated/vpaas
-#rm -rf generated/vpaas/*
-#TARGET_API=vpaas lucybot build --prerender --destination generated/vpaas
+echo "Building VPaaS Website..."
+git clone https://github.com/kaltura/developer-platform-generated generated/vpaas
+rm -rf generated/vpaas/*
+TARGET_API=vpaas lucybot build --prerender --destination generated/vpaas
 
 echo "Building OTT Website..."
 git clone https://github.com/kaltura/ott-developer-platform-generated generated/ott
 rm -rf generated/ott/*
-TARGET_API=ott lucybot build --destination generated/ott
+TARGET_API=ott lucybot build --prerender --destination generated/ott
 
-#cd generated/vpaas
-#echo "Commiting build"
-#git add .
-#git commit -a -m "Build site"
-#git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
+cd generated/vpaas
+echo "Commiting VPaaS build"
+git add .
+git commit -a -m "Build site"
+git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
 
-cd generated/ott
-#cd ../ott
-echo "Commiting build"
+cd ../ott
+echo "Commiting OTT build"
 git add .
 git commit -a -m "Build site"
 git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/ott-developer-platform-generated HEAD:master >> /dev/null 2>&1
