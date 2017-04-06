55c27a9b106a394dd81c8ec9f68b60d7c2345980
Travis CI <bobby.brennan@gmail.com>
2017-04-06 15:07:22 +0000

    adjust client lib script for OTT

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -4,16 +4,17 @@ git config --global user.name "Travis CI"
 git config --global user.email "bobby.brennan@gmail.com"
 
 mkdir ./markdown/generated
-./scripts/resources/all.sh
 
 echo "Building VPaaS Website..."
 git clone https://github.com/kaltura/developer-platform-generated generated/vpaas
 rm -rf generated/vpaas/*
+TARGET_API=vpass ./scripts/resources/all.sh
 TARGET_API=vpaas lucybot build --prerender --destination generated/vpaas
 
 echo "Building OTT Website..."
 git clone https://github.com/kaltura/ott-developer-platform-generated generated/ott
 rm -rf generated/ott/*
+TARGET_API=ott ./scripts/resources/all.sh
 TARGET_API=ott lucybot build --prerender --destination generated/ott
 
 cd generated/vpaas
--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -3,13 +3,24 @@ var fs = require('fs');
 var CLIENT_LANGS = ['php5', 'php53', 'php5Zend', 'java', 'csharp', 'ruby', 'python', 'node', 'ajax', 'cli', 'objc', 'android'];
 
 var clientMD = '';
-var genDate = '22-03-2017';
+
+const LINKS = {
+  ott: {
+    genDate: '26-02-2017',
+    baseURL: 'https://tvpapi-us-preprod.ott.kaltura.com/v4_1/clientlibs/',
+  },
+  vpaas: {
+    genDate: '22-03-2017',
+    baseURL: 'https://tvpapi-us-preprod.ott.kaltura.com/v4_1/clientlibs/',
+  }
+}
 
 const MARKDOWN_DIR = __dirname + '/../../markdown';
+const LINK = process.env.TARGET_API === 'ott' ? LINKS.ott : LINKS.vpaas;
 
 CLIENT_LANGS.forEach(function(cl, idx) {
   if (!cl) return;
-  var link = 'http://cdnbakmi.kaltura.com/content/clientlibs/' + cl + '_' + genDate + '.tar.gz';
+  var link = LINK.baseURL + cl + '_' + LINK.genDate + '.tar.gz';
   if (cl === 'node') cl = 'nodejs';
   let offset = idx * -71;
   clientMD += `<a class="client-lib-link ${cl}" data-language="${cl}" href="${link}" style="background-position: ${offset}px"></a>`;
