cb7e15bac9f799f9042821766279d85c724213d6
Bobby Brennan <bobby.brennan@gmail.com>
2018-03-22 20:14:55 +0000

    better KS for cURL code

--- codegen/templates/curl.ejs.sh
+++ codegen/templates/curl.ejs.sh
@@ -7,8 +7,7 @@ KALTURA_SESSION=`curl -X POST https://www.kaltura.com/api_v3/service/session/act
     -d "type=<%- answers.sessionType || 0 %>" \
     -d "partnerId=<%- answers.partnerId || 'YOUR_PARTNER_ID' %>" \
     -d "expiry=<%- answers.expiry || 86400 %>" \
-    -d "format=3"`
-KALTURA_SESSION=`echo $KALTURA_SESSION | sed -e "s/^.*\"\(.*\)\".*$/\1/"`
+    -d "format=1" | sed -e "s:\"::g"`
 <% } -%>
 curl -X POST https://www.kaltura.com/api_v3/service/<%- serviceID %>/action/<%- actionID %> \
     -d "ks=$KALTURA_SESSION" \
--- test/golden/with_plugin/curl.sh
+++ test/golden/with_plugin/curl.sh
@@ -4,7 +4,6 @@ KALTURA_SESSION=`curl -X POST https://www.kaltura.com/api_v3/service/session/act
     -d "type=0" \
     -d "partnerId=YOUR_PARTNER_ID" \
     -d "expiry=86400" \
-    -d "format=3"`
-KALTURA_SESSION=`echo $KALTURA_SESSION | sed -e "s/^.*\"\(.*\)\".*$/\1/"`
+    -d "format=1" | sed -e "s:\"::g"`
 curl -X POST https://www.kaltura.com/api_v3/service/metadata_metadataprofile/action/list \
     -d "ks=$KALTURA_SESSION" \
