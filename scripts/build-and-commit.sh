28fb06d303eb8771bd9a3b2777641c4309f35f06
Bobby Brennan <bobby.brennan@gmail.com>
2018-04-11 18:24:57 +0000

    fix typo

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -19,9 +19,9 @@ TARGET_API=ovp lucybot build --prerender '^(?!.*\.html$)' --destination generate
 
 cd generated/ovp
 echo "Commiting OVP build"
-git add . >> /dev/nul
+git add . >> /dev/null
 echo "Added everything to commit"
-git commit -a -m "Build site" >> /dev/nul
+git commit -a -m "Build site" >> /dev/null
 echo "Committed everything"
 git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
 echo "Pushed new site"
