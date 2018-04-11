a72ff7b91a41be478e6c27a7696ed03c9d7e822a
Bobby Brennan <bobby.brennan@gmail.com>
2018-04-11 19:16:05 +0000

    enable OTT build

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -11,11 +11,11 @@ rm -rf generated/ovp/*
 TARGET_API=ovp ./scripts/resources/all.sh
 TARGET_API=ovp lucybot build --prerender '^(?!.*\.html$)' --destination generated/ovp
 
-#echo "Building OTT Website..."
-#git clone https://github.com/kaltura/ott-developer-platform-generated generated/ott
-#rm -rf generated/ott/*
-#TARGET_API=ott ./scripts/resources/all.sh
-#TARGET_API=ott lucybot build --prerender --destination generated/ott
+echo "Building OTT Website..."
+git clone https://github.com/kaltura/ott-developer-platform-generated generated/ott
+rm -rf generated/ott/*
+TARGET_API=ott ./scripts/resources/all.sh
+TARGET_API=ott lucybot build --prerender --destination generated/ott
 
 cd generated/ovp
 echo "Commiting OVP build"
@@ -26,8 +26,8 @@ echo "Committed everything"
 git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/developer-platform-generated HEAD:master >> /dev/null 2>&1
 echo "Pushed new site"
 
-#cd ../ott
-#echo "Commiting OTT build"
-#git add .
-#git commit -a -m "Build site"
-#git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/ott-developer-platform-generated HEAD:master >> /dev/null 2>&1
+cd ../ott
+echo "Commiting OTT build"
+git add . >> /dev/null
+git commit -a -m "Build site" >> /dev/null
+git push -q -u https://$GITHUB_ACCESS_TOKEN@github.com/kaltura/ott-developer-platform-generated HEAD:master >> /dev/null 2>&1
