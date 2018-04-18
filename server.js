d57efad887842962c2f14abc792022397b99afb4
Bobby Brennan <bobby.brennan@gmail.com>
2018-04-18 15:22:54 +0000

    show 404 page instead of errors

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var http = require('http');
 var https = require('https');
 var path = require('path');
 var fs = require('fs');
+const TARGET_API = process.env.TARGET_API || 'ovp';
+const STATIC_DIR = __dirname + '/generated/' + TARGET_API;
+const DEFAULT_INDEX = fs.readFileSync(path.join(STATIC_DIR, 'default_index.html'), 'utf8');
 
 var App = Express();
 App.use(require('compression')());
@@ -56,24 +59,12 @@ v4Redirects.forEach(redirect => {
   })
 })
 
-const TARGET_API = process.env.TARGET_API || 'ovp';
 
-const STATIC_DIR = __dirname + '/generated/' + TARGET_API;
 App.use(Express.static(STATIC_DIR));
 
 const GH_PAGES_BASE = '/kaltura-recipes-v3';
 App.use(GH_PAGES_BASE, (req, res, next) => {
   res.redirect(req.originalUrl.substring(GH_PAGES_BASE.length));
-})
-
-let basePages = ['/api-docs', '/console', '/workflows', '/new-workflow', '/preview-workflow'];
-basePages = basePages.map(r => r + '*');
-App.get(basePages, (req, res) => {
-  fs.readFile(path.join(STATIC_DIR, 'default_index.html'), 'utf8', (err, index) => {
-    if (err) return res.status(500).end();
-    res.set('Content-Type', 'text/html');
-    res.send(index);
-  })
 })
 
 App.use('/discussion', require('./routes/discussion'));
@@ -83,6 +74,11 @@ if (process.env.TARGET_API === 'ott') {
   App.use('/auth', require('./routes/partner-auth.js'));
 }
 App.use('/github', require('./routes/github.js'));
+
+App.get('*', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.send(DEFAULT_INDEX);
+})
 
 if (process.env.DEVELOPMENT || process.env.NO_SSL) {
   console.log('----DEVELOPMENT ENVIRONMENT----');
@@ -107,6 +103,7 @@ if (process.env.DEVELOPMENT || process.env.NO_SSL) {
     res.end();
   }).listen(80);
 }
+
 App.use((err, req, res, next) => {
   console.error(err);
   next();
