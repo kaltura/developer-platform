1cab0eeb0140fa55533239de6915b7a557ced1a4
Bobby Brennan <bobby.brennan@gmail.com>
2019-03-19 17:52:49 +0000

    camelCase plugins in python

--- test/golden/abstract_class/python.py
+++ test/golden/abstract_class/python.py
@@ -1,5 +1,5 @@
 
 cue_point = None 
 
-result = client.cuepoint.cuePoint.add(cue_point)
+result = client.cuePoint.cuePoint.add(cue_point)
 print(result)
--- test/golden/add_question_cuepoint/python.py
+++ test/golden/add_question_cuepoint/python.py
@@ -3,5 +3,5 @@ cue_point = KalturaQuestionCuePoint()
 cue_point.entryId = "0_mej0it92"
 cue_point.question = "hello world"
 
-result = client.cuepoint.cuePoint.add(cue_point)
+result = client.cuePoint.cuePoint.add(cue_point)
 print(result)
