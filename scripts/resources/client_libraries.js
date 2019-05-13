135fb834776822684ceffd289ea4f98484ae646e
Jess Portnoy <jess.portnoy@kaltura.com>
2019-05-13 14:14:05 +0100

    14.20.0 released.

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v14.18.0';
+var version = 'v14.20.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
