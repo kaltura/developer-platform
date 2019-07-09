f9b8ba49a695aab6ac5fa871bab7a8a24bc2a109
Bobby Brennan <bobby.brennan@gmail.com>
2019-07-09 12:54:41 +0000

    python fixes

--- test/golden/implicit_object_type/python.py
+++ test/golden/implicit_object_type/python.py
@@ -3,9 +3,9 @@ search_params = KalturaESearchEntryParams()
 search_params.searchOperator = KalturaESearchEntryOperator()
 search_params.searchOperator.operator = KalturaESearchOperatorType.AND_OP
 search_params.searchOperator.searchItems = []
-search_params.searchOperator.searchItems[0] = KalturaESearchCaptionItem()
+search_params.searchOperator.searchItems.append(KalturaESearchCaptionItem())
 search_params.searchOperator.searchItems[0].searchTerm = "asdf"
 pager = KalturaPager()
 
-result = client.elasticsearch.eSearch.searchEntry(search_params, pager)
+result = client.elasticSearch.eSearch.searchEntry(search_params, pager)
 print(result)
