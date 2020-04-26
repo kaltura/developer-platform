9c171e1a579bd6219569446cb09d5c241c843a7e
Jess Portnoy <jess.portnoy@kaltura.com>
2020-04-26 11:29:39 +0000

    .

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v16.0.1';
+var version = 'v16.0.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
