02d6893f96dfd0ae2a1f1987729bd4e317b57f5b
Travis CI <bobby.brennan@gmail.com>
2020-10-13 12:15:25 +0100

    16.9.0 released

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v16.8.5.1';
+var version = 'v16.9.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
