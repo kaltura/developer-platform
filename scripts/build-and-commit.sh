a1524f30e54018cd50874761abbb601ad5718d3e
Jess Portnoy <jess.portnoy@kaltura.com>
2021-01-07 13:29:10 +0000

    [full build] - fix full pre-render condition

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -8,7 +8,7 @@ mkdir ./markdown/generated
 echo "Building OVP Website..."
 git clone https://github.com/kaltura/developer-platform-generated generated/ovp
 full_prerender=0
-if [ "$(git log -1 --pretty=format:'%an %s')" == *"[full build]"* ]; then
+if git log -1 --pretty=format:'%s' |grep -iq "[full build]"; then
   rm -rf generated/ovp/*
   full_prerender=1
   echo "Full build requested. All docs will be rendered anew."
