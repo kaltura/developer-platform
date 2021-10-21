a17ab50b12ac3eb23696af741f94a2a3443eddfc
Robert Brennan <contact@rbren.io>
2021-10-21 21:05:57 +0000

    fix nav highlighting

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -7,6 +7,13 @@ $(window).on('load', function() {
   $('input.g-search-box').on('input', function() {
     window.globalSearch($(this).val());
   });
+  if (window.location.pathname.indexOf('/api-docs') === 0) {
+    $('.subnav.subnav-apis').addClass('active');
+  } else if (window.location.pathname.indexOf('/services') === 0) {
+    $('.subnav.subnav-services').addClass('active');
+  } else {
+    $('.subnav.subnav-guides').addClass('active');
+  }
 
   $('.nav-tabs-b>span').on('click', function(evt) {
     evt.preventDefault();
