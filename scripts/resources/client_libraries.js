7d4f5e66e6165103c5aa5a14c40d1a8de9938a60
Jess Portnoy <jess.portnoy@kaltura.com>
2022-03-12 15:49:34 +0000

    18.1.0 released

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v18.0.0';
+var version = 'v18.1.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
