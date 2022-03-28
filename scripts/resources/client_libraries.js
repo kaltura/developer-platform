6d9e8bd0ff8ba7cc1dc6ad30bb160f9b9fde7d71
Travis CI <jess.portnoy@kaltura.com>
2022-03-28 10:27:27 +0100

    18.2.0 released.

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v18.1.0';
+var version = 'v18.2.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
