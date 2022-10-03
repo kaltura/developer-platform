5ca1ccadcffa423334be6c0f7e72ab1fef0b12c6
Jesse Portnoy <jess.portnoy@kaltura.com>
2022-10-03 22:40:37 +0100

    Revise Ruby template following https://github.com/kaltura/clients-generator/pull/852

--- codegen/templates/ruby.ejs.rb
+++ codegen/templates/ruby.ejs.rb
@@ -6,12 +6,8 @@ config = KalturaConfiguration.new()
 config.service_url = 'https://www.kaltura.com'
 client = KalturaClient.new(config);
 <% if (!noSession) { -%>
-client.ks = client.session_service.start(
-    <%- codegen.constant(answers.secret) %>,
-    <%- codegen.constant(answers.userId) %>,
-    <%- answers.sessionType === 0 ? 'Kaltura::KalturaSessionType::USER' : 'Kaltura::KalturaSessionType::ADMIN' %>,
-    <%- codegen.constant(answers.partnerId) || 'YOUR_PARTNER_ID' %>)
 
+client.ks = client.generate_session_v2(<%- codegen.constant(answers.secret) %>, <%- codegen.constant(answers.userId) %>,<%- answers.sessionType === 0 ? 'Kaltura::KalturaSessionType::USER' : 'Kaltura::KalturaSessionType::ADMIN' %>, <%- codegen.constant(answers.partnerId) || 'YOUR_PARTNER_ID' %>)
 <% } -%>
 <% } -%>
 
