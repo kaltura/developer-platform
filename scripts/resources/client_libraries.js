89d5c3d28279fddc915448eba1c40e071df5af31
Jesse Portnoy <jess.portnoy@kaltura.com>
2023-02-27 13:21:28 +0000

    19.3.0 released to SaaS.

--- scripts/resources/client_libraries.js
+++ scripts/resources/client_libraries.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var ejs = require('ejs');
-var version = 'v19.1.0';
+var version = 'v19.3.0';
 const MARKDOWN_DIR = __dirname + '/../../markdown';
 const TARGET = process.env.TARGET_API || 'ovp';
 const CLIENT_LANGS = {
